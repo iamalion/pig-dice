0083ffbdac62769c7616a6863e086d86d98a533c
Lindsay Warr <iamalion@gmail.com>
2023-06-14 10:27:21 -0700

    Pass test 3

--- README.md
+++ README.md
@@ -49,6 +49,42 @@ Test 2: It will assign points and an ID to a player.
 Code: const name = "Lindsay"
 Expected Output: expected output = Player{name: Lindsay, points: 0, id: 1}
 
+<!--
+function PlayerList() {
+    this.players = {};
+    this.currentId = 0;
+}
+
+PlayerList.prototype.assignId = function() {
+    this.currentId += 1;
+    return this.currentId;
+}
+
+PlayerList.prototype.addPlayer = function (player) {
+    player.id = this.assignId();
+    this.players[player.id] = player;
+}
+
+PlayerList.prototype.findPlayer = function (id) {
+    if (this.players[id] !== undefined) {
+        return this.players[id];
+    }
+    return false;
+}
+
+//Business logic for Player
+function Player(name){
+    this.name = name;
+    this.points = 0;
+}
+-->
+
+### Describe: DiceRoll()
+
+Test 1: It should roll a random number between 1 and 6 (inclusive)
+Code: 
+Expected Output:
+
 ## Setup/Installation Requirements
 
 * _This is a great place_
--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,44 @@
 function PlayerList() {
     this.players = {};
     this.currentId = 0;
-}
+};
 
 PlayerList.prototype.assignId = function() {
     this.currentId += 1;
     return this.currentId;
-}
+};
 
 PlayerList.prototype.addPlayer = function (player) {
     player.id = this.assignId();
     this.players[player.id] = player;
-}
+};
 
 PlayerList.prototype.findPlayer = function (id) {
     if (this.players[id] !== undefined) {
         return this.players[id];
     }
     return false;
-}
+};
 
 //Business logic for Player
 function Player(name){
     this.name = name;
     this.points = 0;
-}
+};
 
-let player1 = new Player ("Casey");
-let player2 = new Player("Lindsay");
-let playerList = new PlayerList();
-playerList.addPlayer(player1);
-playerList.addPlayer(player2);
+//Business logic for DiceRoll
 
-console.log(playerList.players);
+function DiceRoll() {
+    this.numbers = [1, 2, 3, 4, 5, 6];
+};
+
+DiceRoll.prototype.roll = function () {
+    return this.numbers[Math.floor(Math.random() * this.numbers.length)];
+};
+
+const diceRoll = new DiceRoll();
+const result = diceRoll.roll();
+console.log(result);
 
 
 
